c16c63f772a7746b0cb70f660825344b6d5d74c4
gowtham200675443 <145095265+gowtham200675443@users.noreply.github.com>
2025-12-28 12:14:29 +0530

    database.py

--- database.py
+++ database.py
@@ -1,32 +1,4 @@
-# 1from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
-#
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-# engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread': False})
-#
-# SessionLocal= sessionmaker(autocommit=False,autoflush=False, bind=engine)
-# Base = declarative_base()
-#
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-#
-# DATABASE_URL = "sqlite:///./todos.db"
-# # DATABASE_URL = "mysql+pymysql://root:Gowtham2004@%40/@127.0.0.1:3306/todoapp"
-#
-# engine = create_engine(
-#     DATABASE_URL,
-#     connect_args={"check_same_thread": False}  # Only for SQLite
-# )
-#
-# SessionLocal = sessionmaker(
-#     autocommit=False,
-#     autoflush=False,
-#     bind=engine
-# )
-#
-# Base = declarative_base()
-#
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
